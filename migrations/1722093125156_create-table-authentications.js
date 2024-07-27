511ed46c6b508b0a53162c359912daa3fce51cd4
Christianto Kurniawan <christiantokape@gmail.com>
2024-07-28 01:37:21 +0700

    jwt token

---
+++ migrations/1722093125156_create-table-authentications.js
@@ -0,0 +1,20 @@
+exports.up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
--- src/server.js
+++ src/server.js
@@ -4,18 +4,24 @@ const Hapi = require('@hapi/hapi');
 
 const notes = require('./api/notes');
 const users = require('./api/users');
+const authentications = require('./api/authentications');
 
 const NotesService = require('./services/postgres/NotesService');
 const UsersService = require('./services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 
 const NotesValidator = require('./validator/notes');
 const UsersValidator = require('./validator/users');
+const AuthenticationsValidator = require('./validator/authentications');
+
+const TokenManager = require('./tokenize/TokenManager');
 
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -40,6 +46,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
